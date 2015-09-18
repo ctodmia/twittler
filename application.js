147cc4b4f85e4628b6cf660f0ba4df2926b65fdf
Carine TOdmia <ctodmia@gmail.com>
2015-09-17 19:27:12 -0700

    button adds a test div after the last tweet

--- application.js
+++ application.js
@@ -10,6 +10,9 @@
           $tweet.appendTo($body);
           index -= 1;
         }
+
+
+
         $('<button>Add Tweet</button>').appendTo($body);
         $('<input type="text">').appendTo($body);
         $('button').on('click', function(){
@@ -17,6 +20,23 @@
         });
 
 
+//1. Show the user new tweets somehow. (you can show them automatically
+  //as they are created,or create a button that displays new tweets.)
 
+        //to execute this let us create a button that displays new tweets
+        //we want it so that when the user writes a tweet in the input field
+        //and presses the "add tweet" button
+        //a new $tweet <div> is added below the last tweeted div
+
+        //to make it easy on ourselves we can first make it so 
+        //when the button is clicked a new div tag is added below 
+        //the last tweet
+        //to do this lets create an event handler that listens 
+        //for a button click. 
+
+      $('button').on('click', function(){
+
+        $('div').last().after( '<div>Test me out </div>' );
+      })
 
       });
