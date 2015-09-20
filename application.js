6f293fc4b298900eb1676be9be7f89ae4d10ac15
Carine TOdmia <ctodmia@gmail.com>
2015-09-20 05:33:13 -0700

    currently displays the user that was clicked but its super ugly and has a minor bug related to the users added with the button. Almost done and then we can get to styling

--- application.js
+++ application.js
@@ -6,14 +6,11 @@
         while(index >= 0){
           var tweet = streams.home[index];
           var $tweet = $('<div></div>');
-          $tweet.html('<a href="#"> @' +tweet.user +'</a>' + ': ' + tweet.message+': '+ tweet.created_at);
-
+          $tweet.html('<a class="username" href="#">@' +tweet.user +'</a>' + ': ' + tweet.message+': '+ tweet.created_at);
           $tweet.appendTo($body);
           index -= 1;
-
         }
-       var $tweet = $tweet.html('<a href="#">' + '@' + tweet.user +'</a>');
-       console.log($tweet);
+
 
         $('<button>Add Tweet</button>').appendTo($body);
         $('<input type="text">').appendTo($body);
@@ -43,12 +40,35 @@
 
       })
 
-    
+ 
 
    //great. Now that our button does what we want how can we add tweets based
       //on what the users inputs in the input field?
 
+      //ok great now ever thing works almost perfect now we just have to be able to click on a user and only display that users tweets. 
 
+      $('.username').on('click', function(){
+
+        var index = streams.home.length - 1;
+        
+         // when the link its click the console returns
+        //the appropriate user name 
+        //console.log(this.text);
+        while(index >= 0){
+          //console.log(this.text + ' ' + '@' + (streams.home[index]).user);
+          //$(this).css('color', 'red');
+          //$(this).addClass('active');
+          if(this.text === '@' + (streams.home[index]).user){
+            console.log('its a match');
+            $(this).css('color', 'red');
+          var tweet = streams.home[index];
+          var $tweet = $('<div class="active"></div>');
+          $tweet.html('<a class="username" href="#">@' +tweet.user +'</a>' + ': ' + tweet.message+': '+ tweet.created_at);
+          $tweet.appendTo($body);
+        }
+          index -= 1;
+        }
+      })
 
 
 
